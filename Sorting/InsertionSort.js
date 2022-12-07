f8a27de255fa54f6fae896821e4cf53ab49a14d0
rikkarth <ricardominamendes@outlook.com>
2022-12-07 15:34:10 +0000

    changed variable names

--- Sorting/InsertionSort.js
+++ Sorting/InsertionSort.js
@@ -1,8 +1,8 @@
 function insertionSort(arr) {
-  for (let j = 0; j <= arr.length; j++) {
-    for (let i = 0; i <= arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+  for (let i = 0; i <= arr.length; i++) {
+    for (let j = 0; j <= arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
